e70d740e7027d95ef0e1ae5ec62fd5b21bbd8c91
Shubham Ingole <shubhamingole169@gmail.com>
2025-02-12 22:17:25 +0530

    applayout created by  outlet git status

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,35 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Movie } from "./pages/Movie";
 import { Contact } from "./pages/Contact";
+import AppLayout from "./components/layout/AppLayout";
 
 const App = () =>{
 
   const router = createBrowserRouter([
+    
     {
       path: "/",
-      element: <Home />
+      element: <AppLayout />,
+      children:[
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/about",
+          element: <About />
+        },
+        {
+          path: "movie",
+          element: <Movie />
+        },
+        {
+          path: "contact",
+          element: <Contact/>
+        }
+      ]
     },
-    {
-      path: "/about",
-      element: <About />
-    },
-    {
-      path: "movie",
-      element: <Movie />
-    },
-    {
-      path: "contact",
-      element: <Contact/>
-    }
-  ])
+  ]);
   return <RouterProvider router={router} />
 }
 
