56e4c57c93efbc4b182520ebfcee70ee62ea1512
Shubham Ingole <shubhamingole169@gmail.com>
2025-02-13 21:52:48 +0530

    added loader by useNavigation git status!

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,8 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Footer } from "./Footer"
 import { Header } from "./Header"
+import { Loading } from "./Loading";
 
 const AppLayout = () =>{
+
+     const navition = useNavigation();
+     console.log(navition);
+     if(navition.state === "loading") return <Loading />;
         return(
             <>
                 <Header />
